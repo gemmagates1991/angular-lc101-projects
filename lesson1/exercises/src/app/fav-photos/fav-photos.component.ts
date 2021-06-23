26b4ab9ffc28768e06643d341bb477ef176b3f72
gemmagates1991 <gemmagates1991@gmail.com>
2021-06-23 17:38:41 -0500

    added photo

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  image2 = 'https://en.wikipedia.org/wiki/Stock_photography#/media/File:Frog_on_palm_frond.jpg';
   image3 = '';
 
   constructor() { }
